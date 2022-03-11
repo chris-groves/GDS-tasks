8b604e9210022393d9760da470f8418e3d075ce2
Chris Groves <chris.groves03@gmail.com>
2022-03-11 14:30:15 +0000

    Create index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'home/index'
-  root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "home/index"
+  root "home#index"
 
+  resources :tasks, only: [:index]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
